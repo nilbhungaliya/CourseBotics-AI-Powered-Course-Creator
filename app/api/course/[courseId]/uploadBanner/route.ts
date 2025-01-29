90dfcc04963fcae4e2fe0f2db9393daf50df0902
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-01-29 21:53:44 +0530

    fix uploadBanner

--- app/api/course/[courseId]/uploadBanner/route.ts
+++ app/api/course/[courseId]/uploadBanner/route.ts
@@ -1,29 +1,29 @@
-import db from "@/db"
+import db from "@/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async(req: NextRequest)=>{
-    try {
-        const body = await req.json();
-        const { id, fileUrl } = body;
+export const POST = async (req: NextRequest, args: any) => {
+  try {
+    const courseId = await args.params.courseId;
+    const body = await req.json();
+    const { fileUrl } = body;
 
-        console.log(body);
-        
-        const url = fileUrl.replace("dl=0", "raw=1");
-        // Save the file URL in the database
-        const updatedCourse = await db.courseList.update({
-            where: {
-                id: id, // Assuming courseId is the unique identifier
-            },
-            data: {
-                courseBanner: url, // Store the Dropbox URL here
-            },
-        }); 
+    console.log("Body: " + body);
 
-        return NextResponse.json({ updatedCourse });
+    const url = fileUrl.replace("dl=0", "raw=1");
+    // Save the file URL in the database
+    const updatedCourse = await db.courseList.update({
+      where: {
+        courseId: courseId, // Assuming courseId is the unique identifier
+      },
+      data: {
+        courseBanner: url, // Store the Dropbox URL here
+      },
+    });
+    const courseBanner = updatedCourse.courseBanner;
 
-    } catch (error) {
-        console.error("Error storing file URL:", error);
-        NextResponse.json({ error: 'Failed to store file URL' });
-    }
-}
-
+    return NextResponse.json({ courseBanner });
+  } catch (error) {
+    console.error("Error storing file URL:", error);
+    return NextResponse.json({ error: "Failed to store file URL" });
+  }
+};
