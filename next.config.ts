e58aded7a9b52f9cb65dca7ac8a43db6ac5d6447
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-01-29 21:54:32 +0530

    add packages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['plus.unsplash.com', 'www.dropbox.com',"img.clerk.com"], // Add both domains here
+    domains: ['plus.unsplash.com', 'www.dropbox.com',"img.clerk.com",'res.cloudinary.com'], // Add both domains here
   },
 };
 
